f541d464ebd1cb99d9443c788789b715ad5cc89b
devpoonia <sunilpooniachoudhary@gmail.com>
2018-04-29 21:22:17 -0700

    new
    To Creat Magento 2 Custom Theme:
    
        Creating a Magento theme folder
        Declare your theme
        Composer package
        registration.php file
        Declare preview.jpg
        Basic layout elements
        Layout files types and conventions.

---
+++ Customtheme/custom/registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2018 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::THEME,
+    'frontend/customtheme/custom',
+    __DIR__
+);
